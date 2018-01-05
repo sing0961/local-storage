90ed19972be2a91921080e6b5acce80cad8b4157
sing0961 <32390872+sing0961@users.noreply.github.com>
2018-01-05 08:47:33 -0500

    files
    adding files

---
+++ js/starter.js
@@ -0,0 +1,5 @@
+let contactStarter = [
+	{"fullname":"Harpreet singh","phone":"819-318-0592","email":"harpreetsingh01811@gmail.com"},
+	{"fullname":"Gagandeep ","phone":"819-737-4583","email":"gagandeep9197@gmail.com"},
+	
+];
